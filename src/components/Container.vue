9e41c6f0aad8fc8b493d44314e8adb09ca3d1e0c
nomi <67637212+kwoneunyoung@users.noreply.github.com>
2022-04-14 06:40:59 +0900

    4/14 아침스터디
    layout 설정

---
+++ src/components/Container.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <Post :data="data" />
+    <Post />
+    <Post />
+  </div>
+</template>
+
+<script>
+import Post from './Post.vue'
+
+export default {
+    name : 'Container',
+    components : {
+        Post
+    },
+    props : {
+        data : Array
+    }
+}
+</script>
+
+<style>
+
+</style>
