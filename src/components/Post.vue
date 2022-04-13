5c52912c52d6577b943009c67a4307c1fa497686
nomi <67637212+kwoneunyoung@users.noreply.github.com>
2022-04-14 06:46:27 +0900

    4/14 아침스터디
    data binding

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -1,14 +1,14 @@
 <template>
-    <div class="post">
+    <div class="post" v-for="(item, index) in data" :key="index">
         <div class="post-header">
-        <div class="profile"></div>
-        <span class="profile-name">이름</span>
+            <div class="profile"></div>
+            <span class="profile-name">{{ item.name }}</span>
         </div>
         <div class="post-body"></div>
         <div class="post-content">
-        <p>43 Likes</p>
-        <p><strong>글쓴이아이디</strong> 임시내용</p>
-        <p class="date">May 15</p>
+            <p>{{ item.likes }} Likes</p>
+            <p><strong>{{ item.filter }}</strong> {{ item.content }}</p>
+            <p class="date">{{ item.date }}</p>
         </div>
     </div> 
 </template>
